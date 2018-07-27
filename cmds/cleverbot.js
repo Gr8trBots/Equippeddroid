a842cc797796bae3bd2aeafb7a8fe34eaed0347e
hernikplays <hernik27@gmail.com>
2018-07-27 17:25:39 +0100

    fixed talk cmd

--- cmds/cleverbot.js
+++ cmds/cleverbot.js
@@ -3,7 +3,7 @@ const Discord = module.require("discord.js");
 module.exports.run = async (bot, message, args) => {
     
     const cleverbot = require("cleverbot.io");
-    const clever = new cleverbot("cleverbot.iouser", "cleverbot.iokey");
+    const clever = new cleverbot(process.env.CB_USR, process.env.CB_KEY); //replace with your cleverbot.io username and keys! These are hidden in server
     clever.setNick("Source code");
     clever.create(function(err, session) {
         clever.ask(args.join(' '), function(err, res) {
