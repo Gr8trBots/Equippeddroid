33caaa09cd1212ad2981226decf1a48dfa199ce0
hernikplays <hernik27@gmail.com>
2018-07-27 17:13:01 +0100

    No ping
    Write it in the ann

--- cmds/announce.js
+++ cmds/announce.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, message, args) => {
     .setThumbnail(message.guild.iconURL)
     .setDescription("**Announced by: " + message.author + "**\n\n" + text + "\n")
     .setFooter(`An announcment made at ${message.createdTimestamp}`)
-    .setTitle(`@everyone Announcement: ${title}`)
+    .setTitle(`Announcement: ${title}`)
     .setTimestamp()
 
     let annChannel = message.guild.channels.find(`name`, "announcements");
