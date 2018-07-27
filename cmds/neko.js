f24e71ab3d3c6b29960893a3ef90e316c45e2880
hernikplays <hernik27@gmail.com>
2018-07-27 15:30:45 +0100

    no

--- cmds/neko.js
+++ cmds/neko.js
@@ -4,12 +4,12 @@ const neko = new nekolife();
 
 module.exports.run = async (bot, message, args) => {
     
-    console.log(await neko.getSFWNeko);
+    console.log(await neko.getSFWNekoGif);
 
     let embed = new Discord.RichEmbed()
     .setAuthor(message.author.username)
     .setDescription("*meow*")
-    .setImage(await neko.getSFWNeko)
+    .setImage(await neko.getSFWNekoGif)
     .addField("(^・w・^)", "Here is your Neko!")
     
 
