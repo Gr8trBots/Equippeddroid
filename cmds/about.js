e5930edfb02a8be6379594579571248a5e6fc6e9
hernikplays <hernik27@gmail.com>
2018-07-29 21:51:00 +0100

    Added About command

---
+++ cmds/about.js
@@ -0,0 +1,24 @@
+const Discord = module.require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    
+   let m = new Discord.RichEmbed()
+    .setTitle("About")
+    .setDescription("Here is some info about this bot!")
+    .setColor("RANDOM")
+    .addField("Bot Name", "EquippedDroid", true)
+    .addField("Bot created by", "hernikplays#4673", true)
+    .addField("Prefix", "e!", true)
+    .addField("Open Source", "[Yes](https://github.com/discordies/Equippeddroid)",true)
+    .addField("OS License", "AGPL-v3.0", true)
+    .addField("Moderating", `${bot.guilds.size} servers`, true)
+    .addField("Uptime", `${bot.uptime}`, true)
+    .addField("Voting Links", "[DiscordBotList](https://discordbots.org/bot/470989648747954176/vote)\n[Listcord](https://listcord.com/bot/470989648747954176)", true)
+    .setFooter("Thanks for using EquippedDroid!", "https://bot.hernikplays.tk/yeetcon.png")
+    message.channel.send(m).then(msg => {msg.delete(100000)});
+  
+}
+
+module.exports.help = {
+    name: "about"
+}
--- cmds/help.js
+++ cmds/help.js
@@ -26,6 +26,7 @@ module.exports.run = async (bot, message, args) => {
             .addField("8ball <question>", "Ask the magic 8ball!")
             .addField("ann <title> <text>", "Announce something")
             .addField("tr [from:iso] [to:iso] [sentence]", "Translates sentence")
+            .addField("about", "Shows some info about the bot")
             .setFooter(`The bot is still in development -- Prefix to all commands is e! -- <required> [optional]`)
 
             message.channel.send({embed: embed});
