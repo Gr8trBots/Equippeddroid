5081a73c8dd60c32376cc0e1e6ec77f98aefbc2a
hernikplays <hernik27@gmail.com>
2018-07-30 16:29:08 +0100

    bot cert

--- cmds/checkDBL.js
+++ cmds/checkDBL.js
@@ -7,13 +7,10 @@ module.exports.run = async (bot, message, args) => {
     
         if(!args[0]) return  message.channel.send("Please mention a bot or write his ID!");
     dbl.getBot(args[0]).then(bot => {
-        let cert;
-        if(bot.certifiedBot = true){
-            let cert = "Yes";
-            
+        
         let emb = new Discord.RichEmbed()
         .setAuthor(bot.username)
-        .setDescription(`Here is some Info about this bot on [DiscordBotList](https://discordbots.org/bot/${bot.id}`)
+        .setDescription(`Here is some Info about this bot on [DiscordBotList](https://discordbots.org/bot/${bot.id})`)
         .addField("Bot's Username", `${bot.username}#${bot.discriminator}`, true)
         .addField("Bot's Prefix", bot.prefix, true)
         .addField("Short Description", bot.shortdesc, true)
@@ -23,29 +20,11 @@ module.exports.run = async (bot, message, args) => {
         .addField("GitHub Repo", bot.github, true)
         .addField("Owners", bot.owners, true)
         .addField("Upvotes", bot.points, true)
-        .addField("Is the bot certified?", cert, true)
+        .addField("Is the bot certified?", bot.certifiedBot, true)
         .addField("Invite", `[Click to Invite ${bot.username}](${bot.invite})`, true)
         message.channel.send(emb)
-        }
-        else if (bot.certifiedBot = false){
-            let cert = "No";
-            console.log(`${bot.avatar} vs ${bot.defAvatar}`)
-        let emb = new Discord.RichEmbed()
-        .setAuthor(bot.username)
-        .setDescription(`Here is some Info about this bot on [DiscordBotList](https://discordbots.org/bot/${bot.id}`)
-        .addField("Bot's Username", `${bot.username}#${bot.discriminator}`, true)
-        .addField("Bot's Prefix", bot.prefix, true)
-        .addField("Short Description", bot.shortdesc, true)
-        .addField("Library", bot.lib, true)
-        .addField("Tags", bot.tags, true)
-        .addField("Website", `[Website !!May Not Be Safe!!](${bot.website})`, true)
-        .addField("GitHub Repo", bot.github, true)
-        .addField("Owners", bot.owners, true)
-        .addField("Upvotes", bot.points, true)
-        .addField("Is the bot certified?", cert, true)
-        .addField("Invite", `[Click to Invite ${bot.username}](${bot.invite})`, true)
-        message.channel.send(emb)
-        }
+        
+        
         
         
         
