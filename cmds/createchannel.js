3f316acf6a05b4a2269ce9374e6973c090d35d4f
hernikplays <hernik27@gmail.com>
2018-08-05 14:32:49 +0100

    qikfix

--- cmds/createchannel.js
+++ cmds/createchannel.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You need the `MANAGE_CHANNELS` permission to run this comamnd!");
     if(!args[0]) return message.channel.send(`${up} Do you expect me crating a channel out of nothing? Send me the channel name (No spaces)`);
     if(args[0]) return message.channel.send(`${blobdevil} No spaces!`);
-    guild.createChannel(`${args[0]}`, 'text')
+    message.guild.createChannel(`${args[0]}`, 'text')
   .then(console.log)
   .catch(console.error);
   message.channel.send("I have created the channel :)");
