0152bf9348ecb9e3948633d576ff1db987951050
hernikplays <hernik27@gmail.com>
2018-08-05 14:23:48 +0100

    qikfix

--- cmds/announce.js
+++ cmds/announce.js
@@ -28,7 +28,7 @@ module.exports.run = async (bot, message, args) => {
                 .then((collected) => {
                     message.channel.send(`You have sent: ${collected.first().content} and I am creating a channel now!`);
                     guild.createChannel('announcements', 'text', [{
-                        deny: ['SEND_MESSAGES']
+                        deny: ['SEND_MESSAGES'],
                         allow: ['READ_MESSAGES']
                     }])
   .then(message.channel.send("Succesfully created #announcements and you can now adjust it to your needs!"))
