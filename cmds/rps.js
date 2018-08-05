58dca3d2c668fca79d8762b38ef0de1abc7f6ca4
hernikplays <hernik27@gmail.com>
2018-08-05 14:35:27 +0100

    added blobs

--- cmds/rps.js
+++ cmds/rps.js
@@ -1,8 +1,27 @@
 const Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
+  const tie = bot.emojis.find("name", "blobwoah");
+  const rocklose = bot.emojis.find("name", "blobfrown");
+  const paperwin = bot.emojis.find("name", "blobpaper");
+  const scissorslose = bot.emojis.find("name", "blobsad");
+  const scissorswin = bot.emojis.find("name", "blobscissors");
+  const paperlose = bot.emojis.find("name", "blobpapersad");
+  const rockwin = bot.emojis.find("name", "blobrock");
+
+
+
+
+
+
+
+  const errEmbed = new Discord.RichEmbed()
+  .setColor(0xFF0000)
+  .setAuthor('ERROR')
+  .setTitle('Usage: **bl!rps (rock/paper/scissors)**');
+
     var choice = args[0];
+    if(!choice) return message.channel.send(errEmbed);
   if (choice == "paper" || choice == "p") {
     var numb = Math.floor(Math.random() * 100);
     if (numb <= 50) {
@@ -13,11 +32,11 @@ module.exports.run = async (bot, message, args) => {
       var choice2 = "scissors";
     }
     if (choice2 == "scissors") {
-      var response = "I'm choosing **Scissors**! :v: I win!"
+      var response = `${scissorswin} I'm choosing **Scissors**! I win!`
     } else if (choice2 == "paper") {
-      var response = "I'm choosing **Paper**! :hand_splayed: It's a tie!"
+      var response = `${tie} I'm choosing **Paper**! It's a tie!`
     } else {
-      var response = "I'm choosing **Rock**! :punch: You win!"
+      var response = `${rocklose} I'm choosing **Rock**! You win!`
     }
     message.channel.send(response);
   } else if (choice == "rock" || choice == "r") {
@@ -30,11 +49,11 @@ module.exports.run = async (bot, message, args) => {
       var choice2 = "scissors";
     }
     if (choice2 == "paper") {
-      var response = "I'm choosing **Paper**! :hand_splayed: I win!"
+      var response = `${paperwin} I'm choosing **Paper**! :hand_splayed: I win!`
     } else if (choice2 == "rock") {
-      var response = "I'm choosing **Rock**! :punch: It's a tie!"
+      var response = `${tie} I'm choosing **Rock**! :punch: It's a tie!`
     } else {
-      var response = "I'm choosing **Scissors**! :v: You win!"
+      var response = `${scissorslose} I'm choosing **Scissors**! :v: You win!`
     }
     message.channel.send(response);
   } else if (choice == "scissors" || choice == "s") {
@@ -47,11 +66,11 @@ module.exports.run = async (bot, message, args) => {
       var choice2 = "scissors";
     }
     if (choice2 == "rock") {
-      var response = "I'm choosing **Paper**! :hand_splayed: You win!"
+      var response = `${paperlose} I'm choosing **Paper**! :hand_splayed: You win!`
     } else if (choice2 == "scissors") {
-      var response = "I'm choosing **Scissors**! :v: It's a tie!"
+      var response = `${tie} I'm choosing **Scissors**! :v: It's a tie!`
     } else {
-      var response = "I'm choosing **Rock**! :punch: I win!"
+      var response = `${rockwin} I'm choosing **Rock**! :punch: I win!`
     }
     message.channel.send(response);
   } else {
