9c62a94ea669e905c7ae5ec8ce0f3f49177d1162
hernikplays <hernik27@gmail.com>
2018-08-05 14:36:26 +0100

    I feel stupid sometimes

--- cmds/createchannel.js
+++ cmds/createchannel.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
     const de = bot.emojis.find("name", "blobdevil");
     if (!message.member.hasPermission("MANAGE_CHANNELS")) return message.channel.send("You need the `MANAGE_CHANNELS` permission to run this comamnd!");
     if(!args[0]) return message.channel.send(`${up} Do you expect me crating a channel out of nothing? Send me the channel name (No spaces)`);
-    if(args[0]) return message.channel.send(`${de} No spaces!`);
+    if(args[1]) return message.channel.send(`${de} No spaces!`);
     message.guild.createChannel(`${args[0]}`, 'text')
   .then(console.log)
   .catch(console.error);
