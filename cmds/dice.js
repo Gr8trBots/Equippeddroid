46957f9ea5cf6eebd80dc689cb5ea9a0c3f1b9dc
hernikplays <hernik27@gmail.com>
2018-08-08 14:19:35 +0100

    Emojis

--- cmds/dice.js
+++ cmds/dice.js
@@ -2,9 +2,10 @@ const Discord = module.require("discord.js");
 const randomnum = require("unique-random");
 const rand = randomnum(1, 6);
 module.exports.run = async (bot, message, args) => {
-    
+    const de = bot.emojis.find("name", "d6");
+
   await message.channel.send("Rolling the dice...")
-   .then(message => message.edit(`:dice: The dice rolled ${rand()}`));
+   .then(message => message.edit(`${d6} The dice rolled ${rand()}`));
 
   
   
