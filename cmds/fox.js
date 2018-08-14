b8009b35b950cb35b2fd2977531c80e4e9f347e9
hernikplays <hernik27@gmail.com>
2018-08-14 10:56:19 +0100

    ok

--- cmds/fox.js
+++ cmds/fox.js
@@ -3,9 +3,17 @@ var request = require("request");
 module.exports.run = async (bot, message, args) => {
     
     request("https://randomfox.ca/floof/", function(error, response, body) {
-        console.log(body);
+        let plswork = body;
+        console.log(plswork);
       });
-  
+      let removed = plswork.slice(10);
+      let rembefore = removed.slice(0, -40);
+      console.log(removed + " " + rembefore);
+      let em = new Discord.RichEmbed()
+      .setDescription("Here is your fox")
+      .setImage(rembefore)
+      .setFooter("Powered by randomfox.ca")
+      message.channel.send(em);
 }
 
 module.exports.help = {
