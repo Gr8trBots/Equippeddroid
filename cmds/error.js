e5249fcbb08f9af8c030501d8600710e112e4c74
hernikplays <hernik27@gmail.com>
2018-08-17 20:02:56 +0100

    Added Bug tracker link

--- cmds/error.js
+++ cmds/error.js
@@ -3,7 +3,7 @@ let Discord = module.require("discord.js");
 module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
             .setAuthor(message.author.username)
-            .setDescription("If you would like to report an error, or need help with something around the bot, join the support server: https://discord.gg/zfTG2Xv")
+            .setDescription("If you would like to report an error, or need help with something around the bot, join the support server: https://discord.gg/zfTG2Xv or report the bug to our **new** bug tracker [here](https://www.botempire.tk/bugs)")
             .setColor("#000000")
             .setFooter(`Thank you for using EquippedDroid!`)
 
