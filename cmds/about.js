037482be824e05d125c1abb71ee4197d4856f828
hernikplays <hernik27@gmail.com>
2018-08-18 14:07:06 +0100

    Added discordboats.club link

--- cmds/about.js
+++ cmds/about.js
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
     const gh = bot.emojis.find("name", "github");
     const up = bot.emojis.find("name", "upvote");
     const discordbotworld = bot.emojis.find("name", "discordbotworld");
+    const discordboats = bot.emojis.find("name", "discordboats");
     const dbl = new DBL(process.env.DBL_TOKEN, bot); 
     dbl.getBot("470989648747954176").then(botdbl => {
         let uptime = (bot.uptime / 1000 / 60);
@@ -21,10 +22,10 @@ module.exports.run = async (bot, message, args) => {
     .addField("❗ Prefix", "e!", true)
     .addField(`${gh} Open Source`, "[Yes](https://github.com/discordies/Equippeddroid)",true)
     .addField("📃 OS License", "AGPL-v3.0", true)
-    .addField("🔎 Version", "1.21", true)
+    .addField("🔎 Version", "1.22", true)
     .addField("🕵 Moderating", `${bot.guilds.size} servers`, true)
     .addField("⬆ Uptime", `${uptime.toFixed(2)}min`, true)
-    .addField("🔗 Voting Links", `${dblmoji} [DiscordBotList](https://discordbots.org/bot/470989648747954176/vote)\n${lc} [Listcord](https://listcord.com/bot/470989648747954176)\n${botspace} [botlist.space](https://botlist.space/view/470989648747954176)\n${discordbotworld} [discordbot.world](https://discordbot.world/bot/470989648747954176)`, true)
+    .addField("🔗 Voting Links", `${dblmoji} [DiscordBotList](https://discordbots.org/bot/470989648747954176/vote)\n${lc} [Listcord](https://listcord.com/bot/470989648747954176)\n${botspace} [botlist.space](https://botlist.space/view/470989648747954176)\n${discordbotworld} [discordbot.world](https://discordbot.world/bot/470989648747954176)\n${discordboats} [DiscordBoats.club](https://discordboats.club/bot/470989648747954176)`, true)
     .addField(`${up} DBL Upvotes`, botdbl.points, true)
     .setFooter("Thanks for using EquippedDroid!", "https://bot.hernikplays.tk/yeetcon.png")
     message.channel.send(m).then(msg => {msg.delete(100000)});
