5c45ce083d28a3478d47b7a743a640950adfeb22
hernikplays <hernik27@gmail.com>
2018-08-18 22:55:24 +0100

    Trying to implement any user userinfo

--- cmds/userinfo.js
+++ cmds/userinfo.js
@@ -1,7 +1,7 @@
 let Discord = module.require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    
+    if(!args[0]){
         let embed = new Discord.RichEmbed()
             .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL}`)
             .setDescription("This is some of the info I could find:")
@@ -12,7 +12,19 @@ module.exports.run = async (bot, message, args) => {
             .addField("Account created at", `${message.author.createdAt}`)
             .addField("Avatar URL", `${message.author.displayAvatarURL}`)
             .setFooter(`Thanks for using EquippedDroid`, "https://bot.hernikplays.tk/yeetcon.png")
-            
+    }
+    else if(args[0]){
+        let embed = new Discord.RichEmbed()
+            .setAuthor(`${message.author.username}`, `${message.author.displayAvatarURL}`)
+            .setDescription("This is some of the info I could find:")
+            .setColor("#50ed50")
+            .setTimestamp()
+            .addField("User's Full Username", `${user.username}#${user.discriminator}`)
+            .addField("ID", `${user.id}`)
+            .addField("Account created at", `${user.createdAt}`)
+            .addField("Avatar URL", `${user.displayAvatarURL}`)
+            .setFooter(`Thanks for using EquippedDroid`, "https://bot.hernikplays.tk/yeetcon.png")
+    }
             
          message.channel.send({embed: embed});
     
